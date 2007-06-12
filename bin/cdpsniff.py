772d9d4253688f0b0fb6c8c856aea14a4043898c
yardley <yardley@ef009f9b-4a22-0410-a60b-a9d2f4255621>
2007-06-12 16:56:05 +0000

    cdpsniff ported over to the new structure

---
+++ bin/cdpsniff.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#
+# $Id$
+
+import pprint
+import dnet, dpkt
+import dsniff
+import cPickle
+import sys
+
+class CDPSniff(dsniff.Handler):
+    name = 'cdpsniff'
+
+    def setup(self):
+        self.cache = {}
+        self.subscribe('pcap', 'ether dst 01:00:0c:cc:cc:cc', self.recv_pkt)
+
+    def teardown(self):
+        print >>sys.stderr, 'caught %s unique cdp entries' % (len(self.cache), )
+
+    def recv_pkt(self, pc, pkt):
+        eth = dpkt.ethernet.Ethernet(pkt)
+        if eth.src not in self.cache and eth.type == dpkt.ethernet.ETH_TYPE_CDP:
+            d = dict([ (tlv.type, tlv.data) for tlv in eth.cdp.data ])
+            self.cache[eth.src] = d
+            print "%s [%s] - %s [%s]" % (d[1], dnet.eth_ntoa(eth.src), dnet.ip_ntoa(d[2][0].data), d[3])
+            pprint.pprint(d)
+            print
+
+if __name__ == '__main__':
+    dsniff.main()
