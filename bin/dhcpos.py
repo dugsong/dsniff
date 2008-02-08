72b9f419d24a1d71aeee1287dbb5d1ccdb483be4
yardley <yardley@ef009f9b-4a22-0410-a60b-a9d2f4255621>
2008-02-08 22:22:38 +0000

    updated fingerprint signatures and output formatting of dhcpos.py   - fingerprints came from fingerbank.org, duplicates removed   - new fingerprints added by me reformatting output of dhcpos.py because it made me happy. :)

--- bin/dhcpos.py
+++ bin/dhcpos.py
@@ -6,6 +6,8 @@ import dsniff
 
 class DHCPOS(dsniff.Handler):
 
+    types = ('discover', 'offer', 'request', 'decline', 'ack', 'nak', 'release', 'inform')
+
     def setup(self):
         self.fpos = {}
         f = open('share/dhcpos_fingerprints.txt')
@@ -14,6 +16,8 @@ class DHCPOS(dsniff.Handler):
             if not line or line.startswith('#'):
                 continue
             i = line.rfind(',')
+            if self.fpos.has_key(line[:i]):
+                print "duplicate", line[:i], line[i+1:]
             self.fpos[line[:i]] = line[i+1:]
         f.close()
         self.subscribe('pcap', 'udp dst port 67', self.recv_pkt)
@@ -26,8 +30,11 @@ class DHCPOS(dsniff.Handler):
             l = map(ord, opts[dpkt.dhcp.DHCP_OPT_PARAM_REQ])
             fp = ','.join(map(str, l))
             print fp
-            print '%s (%s): %s' % (dnet.eth_ntoa(msg.chaddr),
+            print '%s: %s (%s [%s] - %s):\n%s\n' % (self.types[map(ord, opts[dpkt.dhcp.DHCP_OPT_MSGTYPE])[0]-1],
+                                   dnet.eth_ntoa(msg.chaddr),
                                    dnet.ip_ntoa(ip.src),
+                                   opts.get(dpkt.dhcp.DHCP_OPT_HOSTNAME, 'unknown'),
+                                   opts.get(dpkt.dhcp.DHCP_OPT_VENDOR_ID, 'unknown'),
                                    self.fpos.get(fp, 'UNKNOWN: %s' % fp))
 
 if __name__ == '__main__':
